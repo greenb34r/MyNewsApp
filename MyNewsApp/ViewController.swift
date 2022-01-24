91b85fa1a1527674347a534a6830448a8ae4656c
greenb34r <grn.b34r@gmail.com>
2022-01-24 10:47:06 +0300

    Initial Commit

---
+++ MyNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNewsApp
+//
+//  Created by Алексей Матвеев on 24.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
